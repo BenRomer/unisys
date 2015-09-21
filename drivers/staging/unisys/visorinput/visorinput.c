BenRomer/unisys

commit fbf73c4b4efd5c795c9349f8035aba246d3e593a

staging: unisys: visorinput: no need to track open count
evdev_open_device() and evdev_close_device() (in evdev.c) already track the
open count under lock, and will only call visorinput_open() on the first
open and visorinput_close() on the last close.  So this patch removes the
unnessary logic from visorinput.

drivers/staging/unisys/visorinput/visorinput.c
@@ -226,11 +226,8 @@ static int visorinput_open(struct input_dev *visorinput_dev)
 		       __func__, visorinput_dev);
 		return -EINVAL;
 	}
-	devdata->opened++;
-	dev_dbg(&visorinput_dev->dev, "%s opened %d\n", __func__,
-		devdata->opened);
-	if (devdata->opened == 1)
-		visorbus_enable_channel_interrupts(devdata->dev);
+	dev_dbg(&visorinput_dev->dev, "%s opened\n", __func__);
+	visorbus_enable_channel_interrupts(devdata->dev);
 	return 0;
 }
 
@@ -243,14 +240,8 @@ static void visorinput_close(struct input_dev *visorinput_dev)
 		       __func__, visorinput_dev);
 		return;
 	}
-	if (devdata->opened) {
-		devdata->opened--;
-		dev_dbg(&visorinput_dev->dev, "%s closed %d\n", __func__,
-			devdata->opened);
-		if (devdata->opened == 0)
-			visorbus_disable_channel_interrupts(devdata->dev);
-	} else
-		dev_err(&visorinput_dev->dev, "%s not open\n", __func__);
+	dev_dbg(&visorinput_dev->dev, "%s closed\n", __func__);
+	visorbus_disable_channel_interrupts(devdata->dev);
 }
 
 /*

commit 065f3fcb92d83af9f76ab32823e75dc6f3b8b843

staging: unisys: visorinput: remove unused variable from struct
Fixes: commit fbf73c4b4efd5c795c9349f8035aba246d3e593a

drivers/staging/unisys/visorinput/visorinput.c
@@ -66,7 +66,6 @@ struct visorinput_devdata {
 	struct rw_semaphore lock_visor_dev; /* lock for dev */
 	struct input_dev *visorinput_dev;
 	bool paused;
-	unsigned int opened;
 	unsigned int keycode_table_bytes; /* size of following array */
 	/* for keyboard devices: visorkbd_keycode[] + visorkbd_ext_keycode[] */
 	unsigned char keycode_table[0];