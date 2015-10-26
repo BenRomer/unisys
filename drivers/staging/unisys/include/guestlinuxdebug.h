700d266c94b7de0380f33fad7c5fa2a0bcbf9411
Erik Arfvidson <erik.arfvidson@unisys.com>
2015-10-26 12:05:29 -0700

    staging: unisys: Fix guestlinuxdebug.h comments
    This patch simply cleans up all checkpatch warnings and fixes
    styling
    
    Signed-off-by: Erik Arfvidson <erik.arfvidson@unisys.com>
    Signed-off-by: Benjamin Romer <benjamin.romer@unisys.com>

--- drivers/staging/unisys/include/guestlinuxdebug.h
+++ drivers/staging/unisys/include/guestlinuxdebug.h
@@ -134,8 +134,8 @@ enum event_pc {			/* POSTCODE event identifier tuples */
 
 #define POSTCODE_SEVERITY_ERR DIAG_SEVERITY_ERR
 #define POSTCODE_SEVERITY_WARNING DIAG_SEVERITY_WARNING
-/* TODO-> Info currently doesn't show, so we set info=warning */
 #define POSTCODE_SEVERITY_INFO DIAG_SEVERITY_PRINT
+/* TODO-> Info currently doesn't show, so we set info=warning */
 
 /* example call of POSTCODE_LINUX_2(VISOR_CHIPSET_PC, POSTCODE_SEVERITY_ERR);
  * Please also note that the resulting postcode is in hex, so if you are
