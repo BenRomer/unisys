BenRomer/unisys

commit 7acc0d12eb08d6339aa9141f12c3c0b3c30035ba

staging: unisys: visorbus: fix payload memory usage within controlvm channel
Previously if controlvm message payloads (indicated in controlvm message via
struct controlvm_message.hdr.payload_vm_offset) were contained within the
bounds of the channel memory, we would fail to process any controlvm message
that contained payload data.  Reason is, the request_mem_region() would
fail, because it overlapped the channel memory.  Since request_mem_region()
doesn't actually serve a functional purpose anyway, this was simply
removed.

Also, we were previously neglecting to call iounmap() on memory accessed
via controlvm payloads, thus leaking virtual addresses.  This was corrected.

drivers/staging/unisys/visorbus/visorchipset.c
@@ -400,21 +400,13 @@ parser_init_byte_stream(u64 addr, u32 bytes, bool local, bool *retry)
 		p = __va((unsigned long) (addr));
 		memcpy(ctx->data, p, bytes);
 	} else {
-		void __iomem *mapping;
-
-		if (!request_mem_region(addr, bytes, "visorchipset")) {
-			rc = NULL;
-			goto cleanup;
-		}
-
-		mapping = ioremap_cache(addr, bytes);
+		void __iomem *mapping = ioremap_cache(addr, bytes);
 		if (!mapping) {
-			release_mem_region(addr, bytes);
 			rc = NULL;
 			goto cleanup;
 		}
 		memcpy_fromio(ctx->data, mapping, bytes);
-		release_mem_region(addr, bytes);
+		iounmap(mapping);
 	}
 
 	ctx->byte_stream = true;

commit 210e1df58179379dcd8b239cc332c0607220914b

staging: unisys: fix checkpatch problem caused by commit 7acc0d1

drivers/staging/unisys/visorbus/visorchipset.c
@@ -402,6 +402,7 @@ parser_init_byte_stream(u64 addr, u32 bytes, bool local, bool *retry)
 		memcpy(ctx->data, p, bytes);
 	} else {
 		void __iomem *mapping = ioremap_cache(addr, bytes);
+
 		if (!mapping) {
 			rc = NULL;
 			goto cleanup;