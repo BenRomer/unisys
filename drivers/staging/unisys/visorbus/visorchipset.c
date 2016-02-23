BenRomer/unisys

commit 835f46dc5eac765199f1797f221442855f1de908

staging: unisys: visorbus: fix payload memory usage within controlvm channel
Previously if controlvm message payloads (indicated in controlvm message via
struct controlvm_message.hdr.payload_vm_offset) were contained within the
bounds of the channel memory, we would fail to process any controlvm message
that contained payload data.  Reason is, the request_mem_region() would
fail, because it overlapped the channel memory.  Since request_mem_region()
doesn't actually serve a functional purpose anyway, this was simply
removed.

drivers/staging/unisys/visorbus/visorchipset.c
@@ -400,21 +400,12 @@ parser_init_byte_stream(u64 addr, u32 bytes, bool local, bool *retry)
 		p = __va((unsigned long)(addr));
 		memcpy(ctx->data, p, bytes);
 	} else {
-		void *mapping;
-
-		if (!request_mem_region(addr, bytes, "visorchipset")) {
-			rc = NULL;
-			goto cleanup;
-		}
-
-		mapping = memremap(addr, bytes, MEMREMAP_WB);
+		void *mapping = memremap(addr, bytes, MEMREMAP_WB);
 		if (!mapping) {
-			release_mem_region(addr, bytes);
 			rc = NULL;
 			goto cleanup;
 		}
 		memcpy(ctx->data, mapping, bytes);
-		release_mem_region(addr, bytes);
 		memunmap(mapping);
 	}
 

commit bfc6acd7fed4c8d7a5026c18dccbacce64a129bc

staging: unisys: fix checkpatch problem caused by commit 835f46d

drivers/staging/unisys/visorbus/visorchipset.c
@@ -402,6 +402,7 @@ parser_init_byte_stream(u64 addr, u32 bytes, bool local, bool *retry)
 		memcpy(ctx->data, p, bytes);
 	} else {
 		void *mapping = memremap(addr, bytes, MEMREMAP_WB);
+
 		if (!mapping) {
 			rc = NULL;
 			goto cleanup;